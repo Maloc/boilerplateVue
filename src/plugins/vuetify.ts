eac31af73677db12aab6ea9b472dba3819497873
Guillaume <ghoarau@maltem.com>
2019-04-08 17:59:31 +0200

    Lint project

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,6 +1,6 @@
-import Vue from 'vue'
-import Vuetify, { VFlex, VLayout, VContainer, VImg } from 'vuetify/lib'
-import 'vuetify/src/stylus/app.styl'
+import Vue from 'vue';
+import Vuetify, { VFlex, VLayout, VContainer, VImg } from 'vuetify/lib';
+import 'vuetify/src/stylus/app.styl';
 
 Vue.use(Vuetify, {
   components: { VFlex, VLayout, VContainer, VImg },
@@ -11,10 +11,10 @@ Vue.use(Vuetify, {
     error: '#FF5252',
     info: '#2196F3',
     success: '#4CAF50',
-    warning: '#FFC107'
+    warning: '#FFC107',
   },
   options: {
-    customProperties: true
+    customProperties: true,
   },
   iconfont: 'md',
-})
+});
